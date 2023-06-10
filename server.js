79b889f715bb251132974e7aacbb10d716d79990
JynoX <40642679+JynoX@users.noreply.github.com>
2023-06-10 16:46:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ const campiRouter = require('./routes/campi');
 
 const app=express();
 
-var corsOptions={
-    origin:"http://localhost:8081"
-}
+// var corsOptions={
+//     origin:"http://localhost:8081"
+// }
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
